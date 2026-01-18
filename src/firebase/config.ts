3684979a455066a992c93d2a023fc0d5a7d8f993
Robin Pierre-justin <robinPJ45@gmail.com>
2026-01-18 11:31:24 +0000

    Try fixing this error: `Console FirebaseError: Firebase Storage: No defa

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:801756532533:web:b3e4f1dcb101f88103c67c",
   "apiKey": "AIzaSyBlyKnScgbi9cqHDhFK3SEuajb0yf5VnlI",
   "authDomain": "studio-6686204297-42e5a.firebaseapp.com",
+  "storageBucket": "studio-6686204297-42e5a.appspot.com",
   "measurementId": "",
   "messagingSenderId": "801756532533"
 };
